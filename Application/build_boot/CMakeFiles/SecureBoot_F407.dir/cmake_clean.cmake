4c6c1e5c594f0039fbfd198478d5011f8b2fb8ed
Shelton Ngwenya <shelt15.nn@gmail.com>
2025-06-03 02:25:00 +0100

    created 2 stm32cubemx projects for bootloader and application
    CubeMX won't overwrite shared files
    Each firmware gets its own build, linker, and source logic
    You wont get duplicate main.c conflicts

---
+++ Application/build_boot/CMakeFiles/SecureBoot_F407.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "SecureBoot_F407.map"
+  "CMakeFiles/SecureBoot_F407.dir/Core/Src/main.c.obj"
+  "CMakeFiles/SecureBoot_F407.dir/Core/Src/main.c.obj.d"
+  "CMakeFiles/SecureBoot_F407.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/SecureBoot_F407.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "CMakeFiles/SecureBoot_F407.dir/Core/Src/stm32f4xx_it.c.obj"
+  "CMakeFiles/SecureBoot_F407.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "CMakeFiles/SecureBoot_F407.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/SecureBoot_F407.dir/Core/Src/syscalls.c.obj.d"
+  "CMakeFiles/SecureBoot_F407.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/SecureBoot_F407.dir/Core/Src/sysmem.c.obj.d"
+  "CMakeFiles/SecureBoot_F407.dir/startup_stm32f407xx.s.obj"
+  "SecureBoot_F407.elf"
+  "SecureBoot_F407.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/SecureBoot_F407.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Application/build_boot/CMakeFiles/binaries.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binaries"
+  "SecureBoot_F407.bin"
+  "SecureBoot_F407.hex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/binaries.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
